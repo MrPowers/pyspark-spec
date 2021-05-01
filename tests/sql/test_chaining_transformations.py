5df6f40e74688fa8027a966d6737683eabeabeb3
MrPowers <matthewkevinpowers@gmail.com>
2021-05-01 10:07:56 -0400

    Add exists and forall

---
+++ tests/sql/test_chaining_transformations.py
@@ -0,0 +1,16 @@
+import pytest
+from spark import *
+
+from pyspark.sql.functions import col, lit, concat
+
+class TestChainingTransformations(object):
+
+    def test_funify(self):
+        def funify(col_name, str):
+            def _(df):
+                return df.withColumn("funified", concat(col(col_name), lit(" "), lit(str)))
+            return _
+        data = [("colombia",), ("brasil",), (None,)]
+        df = spark.createDataFrame(data, ["country"])
+        df.show(truncate=100)
+        df.transform(funify("country", "is super fun!")).show(truncate=False)
--- tests/sql/test_functions.py
+++ tests/sql/test_functions.py
@@ -3,25 +3,44 @@ from spark import *
 import datetime
 
 from pyspark.sql.functions import *
+from chispa.dataframe_comparer import *
 
 class TestFunctions(object):
+
+    def test_exists(self):
+        df = spark.createDataFrame(
+            [(["a", "b", "c"],), (["x", "y", "z"],)], ["some_arr"]
+        )
+        equals_b = lambda e: e == "b"
+        res = df.withColumn("has_b", exists(col("some_arr"), equals_b))
+        expected = spark.createDataFrame(
+            [(["a", "b", "c"], True), (["x", "y", "z"], False)], ["some_arr", "has_b"]
+        )
+        assert_df_equality(res, expected)
+
+
+    def test_forall(self):
+        df = spark.createDataFrame(
+            [([1, 2, 3],), ([2, 6, 12],)], ["some_arr"]
+        )
+        is_even = lambda e: e % 2 == 0
+        res = df.withColumn("all_even", forall(col("some_arr"), is_even))
+        expected = spark.createDataFrame(
+            [([1, 2, 3], False), ([2, 6, 12], True)], ["some_arr", "all_even"]
+        )
+        assert_df_equality(res, expected)
+
 
     def test_year(self):
         sourceDF = spark.createDataFrame(
             [("jose", datetime.date(2017, 1, 1)), ("li", datetime.date(2015, 7, 6))], ["name", "birthdate"]
         )
-
         actualDF = sourceDF.withColumn("birthyear", year(col("birthdate")))
-
         expectedDF = spark.createDataFrame(
             [
                 ("jose", datetime.date(2017, 1, 1), 2017),
                 ("li", datetime.date(2015, 7, 6), 2015)
             ],
-            [
-                "name",
-                "birthdate"
-            ]
+            ["name", "birthdate"]
         )
-
         assert(expectedDF.collect() == actualDF.collect())
