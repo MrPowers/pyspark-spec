abb112339f3db8e7cff7681cac0bdc86e268c098
MrPowers <matthewkevinpowers@gmail.com>
2021-05-04 10:58:39 -0400

    Add union and unionByName

--- tests/sql/test_dataframe.py
+++ tests/sql/test_dataframe.py
@@ -1,6 +1,7 @@
 import pytest
 from spark import *
 
+from chispa.dataframe_comparer import *
 from pyspark.sql.functions import col
 
 
@@ -142,10 +143,57 @@ class TestDataFrame(object):
     def test_select_with_multiple_string_arguments(self):
         data = [("jose", 1, "mexico"), ("li", 2, "china"), ("sandy", 3, "usa")]
         source_df = spark.createDataFrame(data, ["name", "age", "country"])
-
         actual_df = source_df.select("age", "name")
-
         data = [(1, "jose"), (2, "li"), (3, "sandy")]
         expected_df = spark.createDataFrame(data, ["age", "name"])
-
         assert sorted(actual_df.collect()) == sorted(expected_df.collect())
+
+
+    def test_union(self):
+        americans = spark.createDataFrame(
+            [("bob", 42), ("lisa", 59)], ["first_name", "age"]
+        )
+        colombians = spark.createDataFrame(
+            [("maria", 20), ("camilo", 31)], ["first_name", "age"]
+        )
+        res = americans.union(colombians)
+        expected = spark.createDataFrame(
+            [("bob", 42), ("lisa", 59), ("maria", 20), ("camilo", 31)], ["first_name", "age"]
+        )
+        assert_df_equality(res, expected)
+        # this is a bug that definitely should not exist
+        # brasilans = spark.createDataFrame(
+        #     [(33, "tiago"), (36, "lilly")], ["age", "first_name"]
+        # )
+        # brasilans.show()
+        # americans.union(brasilans).show()
+        # americans.printSchema()
+        # brasilans.printSchema()
+
+
+    def test_unionByName(self):
+        americans = spark.createDataFrame(
+            [("bob", 42), ("lisa", 59)], ["first_name", "age"]
+        )
+        brasilans = spark.createDataFrame(
+            [(33, "tiago"), (36, "lilly")], ["age", "first_name"]
+        )
+        res = americans.unionByName(brasilans)
+        expected = spark.createDataFrame(
+            [("bob", 42), ("lisa", 59), ("tiago", 33), ("lilly", 36)], ["first_name", "age"]
+        )
+        assert_df_equality(res, expected)
+
+
+    def test_unionByName_with_option(self):
+        americans = spark.createDataFrame(
+            [("bob", 42), ("lisa", 59)], ["first_name", "age"]
+        )
+        indians = spark.createDataFrame(
+            [(55, "arjun", "cricket"), (5, "ira", "playing")], ["age", "first_name", "hobby"]
+        )
+        res = americans.unionByName(indians, allowMissingColumns=True)
+        expected = spark.createDataFrame(
+            [("bob", 42, None), ("lisa", 59, None), ("arjun", 55, "cricket"), ("ira", 5, "playing")], ["first_name", "age", "hobby"]
+        )
+        assert_df_equality(res, expected)
